b6d4fef1ea4744730c2d580c655df37bb147bea4
Danilo Nascimento <danilodelete@hotmail.com>
2019-09-13 10:55:30 -0300

    fix: chamado

--- helpDeskBackend/app/models/tchamado.php
+++ helpDeskBackend/app/models/tchamado.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class tchamado extends Model
 {
     protected $table = 'tchamado';
-    protected $fillable = ['descricao', 'data', 'ti', 'tombo', 'id_tecnico', "id_usuario", 'id_setor'];
+    protected $fillable = ['descricao', 'data', 'ti', 'tombo', 'id_tecnico', "id_usuario", 'id_setor', 'id_problema'];
     public $timestamps = false;
 
     //protected $guarded = ['updated_at'];
